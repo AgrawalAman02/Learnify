2ad2aad3a1b186183f6974292a0ebdb2a212c23c
AgrawalAman02 <amanhzb2003@gmail.com>
2024-12-18 19:46:59 +0530

    loaderpage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,27 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import appStore from './store/appStore.jsx';
-import { Toaster } from './components/ui/sonner.jsx';
-import { ThemeProvider } from './components/themeProvider.jsx';
+import { Children, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import appStore from "./store/appStore.jsx";
+import { Toaster } from "./components/ui/sonner.jsx";
+import { ThemeProvider } from "./components/themeProvider.jsx";
+import { useGetUserQuery } from "./apis/profileApi.jsx";
+import LoaderSpinner from "./pages/LoaderSpinner.jsx";
 
-createRoot(document.getElementById('root')).render(
+const Custom = ({ children }) => {
+  const { isLoading } = useGetUserQuery();
+  return <>{isLoading ? <LoaderSpinner/> : <>{children}</>}</>;
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Provider store={appStore}>
-        <App />
-        <Toaster/>
+        <Custom>
+          <App />
+          <Toaster />
+        </Custom>
       </Provider>
     </ThemeProvider>
   </StrictMode>
---
+++ client/src/pages/LoaderSpinner.jsx
@@ -0,0 +1,17 @@
+import { Loader } from 'lucide-react'
+import React from 'react'
+
+const LoaderSpinner = () => {
+  return (
+    <div>
+        <div className='flex flex-xol items-center justify-center min-h-screen bg-gray-50'>
+            <Loader className= "animate-spin h-16 w-16 text-blue-600" />
+            <p className='mt-4 text-lg font-semibold text-gray-700'>Loading, Please wait...</p>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default LoaderSpinner
