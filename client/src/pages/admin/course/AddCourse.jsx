64db94aa90422eacf2b9758555cb4b87e0ef7552
AgrawalAman02 <amanhzb2003@gmail.com>
2024-12-19 22:51:16 +0530

    initiate the admin frontend

---
+++ client/src/pages/admin/course/AddCourse.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddCourse = () => {
+  return (
+    <div className="p-4 flex flex-col items-start flex-1">
+      <p className="text-xl font-bold">Do You wanna add new course?</p>
+      <p className="text-sm font-medium">
+        {" "}
+        So you are at right page. Lets add new course. Please provide full
+        description of the course{" "}
+      </p>
+
+      
+    </div>
+  );
+};
+
+export default AddCourse;
