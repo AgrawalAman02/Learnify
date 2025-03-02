9124481be814dd98da0baa68db332260ff3e0452
AgrawalAman02 <amanhzb2003@gmail.com>
2025-03-03 02:24:25 +0530

    feat: add sales and revenue cards with line chart visualization in Dashboard

--- client/src/pages/admin/DashBoard.jsx
+++ client/src/pages/admin/DashBoard.jsx
@@ -1,22 +1,115 @@
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
-import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const DashBoard = () => {
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   return (
-    <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-      <Card className="w-56 h-24 flex items-center border-gray-500">
-        <CardHeader>
-          <CardTitle>Total Sales</CardTitle>
-        </CardHeader>
-      </Card>
+    <div className="flex flex-col gap-10 p-4 ">
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <Card className=" border-gray-500 shadow-gray-400 dark:shadow-gray-900 shadow-lg hover:shadow-xl transition-shadow   duration-300  ">
+          <CardHeader>
+            <CardTitle className="text-xl font-semibold text-gray-700">
+              Total Sales
+            </CardTitle>
+          </CardHeader>
 
-      <Card className =" flex items-center border-gray-500">
+          <CardContent>
+            <p className="text-3xl font-bold text-blue-600">400</p>
+          </CardContent>
+        </Card>
+
+        <Card className="  border-gray-500 shadow-gray-400 dark:shadow-gray-900 shadow-lg hover:shadow-xl transition-shadow   duration-300">
+          <CardHeader>
+            <CardTitle className="text-xl font-semibold text-gray-700">
+              Total Revenue
+            </CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-3xl font-bold text-blue-600">1200</p>
+          </CardContent>
+        </Card>
+      </div>
+
+      <Card className="  border-gray-500 shadow-gray-400 dark:shadow-gray-900 shadow-lg hover:shadow-xl transition-shadow   duration-300">
         <CardHeader>
-          <CardTitle>Total Sales</CardTitle>
+          <CardTitle className="text-xl font-semibold text-gray-700">
+            Course Prices
+          </CardTitle>
         </CardHeader>
+
+        <CardContent>
+          <ResponsiveContainer width={"100%"} height={250}>
+            <LineChart
+              width={400}
+              height={400}
+              data={data}
+              margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+            >
+              <XAxis dataKey="name" />
+              <Tooltip />
+              <CartesianGrid stroke="#f5f5f5" />
+              <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
+              <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
+            </LineChart>
+          </ResponsiveContainer>
+        </CardContent>
       </Card>
     </div>
-  ) 
-}
+  );
+};
 
-export default DashBoard
+export default DashBoard;
