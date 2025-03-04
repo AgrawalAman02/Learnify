8f5410a5dc6558e3285a843c370a4b27b5ab14ab
AgrawalAman02 <amanhzb2003@gmail.com>
2025-03-04 13:10:31 +0530

    fix: debuging (add refetch logic and enhance navigation)

--- client/src/components/ProtectPurchasedPage.jsx
+++ client/src/components/ProtectPurchasedPage.jsx
@@ -1,14 +1,23 @@
 import { useGetCoursePurchasedDetailsQuery } from '@/apis/courseApi';
 import LoaderSpinner from '@/pages/LoaderSpinner';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 
 export const ProtectPurchasedPage = ({children}) => {
   const {courseId} = useParams();
 
-  const {data , isLoading} = useGetCoursePurchasedDetailsQuery(courseId);
+  const {data , isLoading,refetch} = useGetCoursePurchasedDetailsQuery(courseId);
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
 
   if(isLoading) return <LoaderSpinner/>
 
-  return data?.isPurchased ? children : <Navigate to={`/courseDetails/${courseId}`} />
+  const isPendingPayment = window.location.pathname.includes('/success/video');
+
+  return (data?.isPurchased || isPendingPayment) ? 
+  children : 
+  <Navigate to={`/courseDetails/${courseId}`} />;
 }
