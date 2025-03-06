16ef6ae9815c8c74466e8fe4dfe624527c0396c3
AgrawalAman02 <amanhzb2003@gmail.com>
2025-03-06 23:28:35 +0530

    feat: implement password reset functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { sendMail } from "../utils/email.js";
 import { validateSignUP } from "../utils/validate.js";
 import bcrypt from "bcryptjs";
 import validator from "validator";
+import crypto from 'crypto'
 
 export const signUp = async (req, res) => {
   try {
@@ -128,10 +129,57 @@ export const forgotPassword = async (req,res)=>{
     }
 
   } catch (error) {
-    
+    return res.status(500).json({
+      success: false,
+      message: `ERROR : ${error.message} `,
+    });
   }
 }
 
 export const resetPassword = async (req,res)=>{
+  try {
+    const token = crypto.createHash('sha256').update(req.params.token).digest("hex");
+    const user = await User.findOne({passwordResetToken : token, passwordResetTokenExpires :{$gt : Date.now()}});
+  
+    if(!user) return res.status(400).json({
+      success : false,
+      message : "Can't verify your Password. May the link expires Please try again...",
+    });
+  
+    const {password , confirmPassword } = req.body;
+    if(password !== confirmPassword) return res.status(400).json({
+      success : false ,
+      message : "Password not matched...",
+    });
+  
+    user.password = await bcrypt.hash(password,10);
+    user.passwordResetToken = undefined;
+    user.passwordResetTokenExpires = undefined;
+  
+    user.passwordChangedAt = Date.now();
+    
+    const savedUser = await user.save();
 
+    // Generate JWT token and set cookie after signup
+    const loginToken = await savedUser.getJWT();
+    res.cookie("token", loginToken, {
+      expires: new Date(Date.now() + 8 * 3600000),
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+  
+    return res.status(200).json({
+      savedUser,
+      success : true,
+      message : "Password Changed Successfully",
+      // token
+    });   
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: `ERROR : ${error.message} `,
+    });
+  }
 }
