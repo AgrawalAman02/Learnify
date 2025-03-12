f71987cd970bf89f7f4373b5be4bf4578d8cf3cd
AgrawalAman02 <amanhzb2003@gmail.com>
2025-03-12 09:27:09 +0530

    feat : add hover effect

--- client/src/pages/admin/SideBar.jsx
+++ client/src/pages/admin/SideBar.jsx
@@ -27,13 +27,13 @@ const SideBar = () => {
   return (
     <div className="flex">
       <div className="hidden lg:block  w-[250px] sm:w-[300px] space-y-8 border-r border-r-gray-300 dark:border-r-gray-700 p-5 sticky top-0  h-screen">
-        <div className="space-y-6">
-          <Link to="dashboard" className={`flex gap-2 items-center  p-2 ${selected=='dashboard' ? "bg-blue-300 dark:bg-blue-700 rounded-s-xl" : ""}`}  onClick={()=>handleSidebar('dashboard')}>
+        <div className="space-y-4">
+          <Link to="dashboard" className={`flex gap-2 items-center  p-2 pl-4 ${selected=='dashboard' ? "bg-blue-300 dark:bg-blue-700 rounded-s-xl" : "hover:bg-blue-50 rounded-s-xl dark:hover:bg-[#4f4fdd2c]"}`}  onClick={()=>handleSidebar('dashboard')}>
             <ChartNoAxesColumn size={22} />
             <h1>DashBoard</h1>
           </Link>
 
-          <Link to="/admin/course" className={`flex gap-2 items-center p-2  ${selected=='courses' ? " bg-blue-300 dark:bg-blue-700 rounded-s-xl" : ""}`} onClick={()=>handleSidebar('courses')}>
+          <Link to="/admin/course" className={`flex gap-2 items-center p-2 pl-4 ${selected=='courses' ? " bg-blue-300 dark:bg-blue-700 rounded-s-xl" : "hover:bg-blue-50 rounded-s-xl dark:hover:bg-[#4f4fdd2c]"}`} onClick={()=>handleSidebar('courses')}>
             <SquareLibrary size={22} />
             <h1>Courses</h1>
           </Link>
